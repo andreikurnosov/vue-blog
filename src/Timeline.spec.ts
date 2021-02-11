aa4bc5ed234a103895bc09d6f14a09d28bdd110a
andreikurnosov <nixdie@yandex.ru>
2021-02-11 16:09:16 +0700

    sync/async vue tests

--- src/Timeline.spec.ts
+++ src/Timeline.spec.ts
@@ -1,4 +1,5 @@
 import { mount } from '@vue/test-utils'
+import { nextTick } from 'vue'
 import Timeline from './Timeline.vue'
 
 describe('Timeline', () => {
@@ -6,5 +7,23 @@ describe('Timeline', () => {
     const wrapper = mount(Timeline)
 
     expect(wrapper.findAll('[data-test="period"]')).toHaveLength(3)
+  })
+  it('Updates period when clicked', async () => {
+    const wrapper = mount(Timeline)
+
+    const $today = wrapper.findAll('[data-test="period"]')[0]
+    expect($today.classes()).toContain('is-active')
+
+    const $thisWeek = wrapper.findAll('[data-test="period"]')[1]
+    await $thisWeek.trigger('click')
+
+    expect($today.classes()).not.toContain('is-active')
+    expect($thisWeek.classes()).toContain('is-active')
+
+    const $thisMonth = wrapper.findAll('[data-test="period"]')[2]
+    await $thisMonth.trigger('click')
+
+    expect($thisWeek.classes()).not.toContain('is-active')
+    expect($thisMonth.classes()).toContain('is-active')
   })
 })
