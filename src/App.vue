dd90a4b190cb68a05fdc93b7284e447d2c620d37
andreikurnosov <nixdie@yandex.ru>
2021-02-11 15:45:48 +0700

    inital commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <Timeline />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import Timeline from './Timeline.vue'
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    Timeline,
+  },
+})
+</script>
