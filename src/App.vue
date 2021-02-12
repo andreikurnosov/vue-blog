d042cb0ff7268f71bbac5e055036f4d1f03436a5
andreikurnosov <nixdie@yandex.ru>
2021-02-12 14:53:54 +0700

    loading states with suspense

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,29 @@
 <template>
-  <Timeline />
+  <Suspense>
+    <template #default>
+      <Timeline />
+    </template>
+    <template #fallback>
+      <div class="columns">
+        <div class="column is-one-third"></div>
+        <div class="column is-one-third">
+          <Progress />
+        </div>
+        <div class="column is-one-third"></div>
+      </div>
+    </template>
+  </Suspense>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 import Timeline from './Timeline.vue'
-
+import Progress from './Progress.vue'
 export default defineComponent({
   name: 'App',
   components: {
     Timeline,
+    Progress,
   },
 })
 </script>
