ccf16bc41fb559e2b0625982f9f386a44d11ff78
andreikurnosov <nixdie@yandex.ru>
2021-02-12 13:49:11 +0700

    filtering posts with computed

--- src/Timeline.vue
+++ src/Timeline.vue
@@ -11,17 +11,49 @@
         {{ period }}
       </a>
     </p>
+    <a class="panel-block" v-for="post in posts" :key="post.id" data-test="post">
+      <div>
+        <a>{{ post.title }}</a>
+        <div>{{ post.created.format('Do MMM') }}</div>
+      </div>
+    </a>
   </nav>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from 'vue'
-import { Period } from './types'
+import { computed, defineComponent, ref } from 'vue'
+import { Period, Post } from './types'
+import { todayPost, thisWeek, thisMonth } from './mocks'
+import moment from 'moment'
 
 export default defineComponent({
   setup() {
     const periods: Period[] = ['today', 'this week', 'this month']
     const selectedPeriod = ref<Period>('today')
+
+    const posts = computed(() =>
+      [todayPost, thisWeek, thisMonth].filter((post) => {
+        if (
+          selectedPeriod.value === 'today' &&
+          post.created.isAfter(moment().subtract(1, 'day'))
+        ) {
+          return true
+        }
+        if (
+          selectedPeriod.value === 'this week' &&
+          post.created.isAfter(moment().subtract(7, 'day'))
+        ) {
+          return true
+        }
+        if (
+          selectedPeriod.value === 'this month' &&
+          post.created.isAfter(moment().subtract(1, 'month'))
+        ) {
+          return true
+        }
+        return false
+      })
+    )
 
     const setPeriod = (period: Period) => {
       selectedPeriod.value = period
@@ -30,6 +62,7 @@ export default defineComponent({
       periods,
       selectedPeriod,
       setPeriod,
+      posts,
     }
   },
 })
