af28a7141cce836ab43e3ce58d28bc4fd5c8ed51
andreikurnosov <nixdie@yandex.ru>
2021-02-13 08:52:45 +0700

    template refs and content editable

--- src/PostWriter.vue
+++ src/PostWriter.vue
@@ -1,19 +1,27 @@
 <template>
-  <div class="columns">
-    <div class="column">
-      <div class="field">
-        <div class="label">Post title</div>
-        <div class="control">
-          <input type="text" v-model="title" class="input" />
-          {{ title }}
+  <div>
+    <div class="columns">
+      <div class="column">
+        <div class="field">
+          <div class="label">Post title</div>
+          <div class="control">
+            <input type="text" v-model="title" class="input" />
+            {{ title }}
+          </div>
         </div>
       </div>
+    </div>
+    <div class="columns">
+      <div class="column is-one-half">
+        <div contenteditable id="markdown" ref="contentEditable" />
+      </div>
+      <div class="column is-one-half"></div>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from 'vue'
+import { defineComponent, onMounted, ref } from 'vue'
 import { Post } from './types'
 
 export default defineComponent({
@@ -26,8 +34,15 @@ export default defineComponent({
   },
   setup(props) {
     const title = ref(props.post.title)
+    const contentEditable = ref<null | HTMLDivElement>(null)
+
+    console.log(contentEditable.value)
+    onMounted(() => {
+      console.log(contentEditable.value)
+    })
     return {
       title,
+      contentEditable,
     }
   },
 })
