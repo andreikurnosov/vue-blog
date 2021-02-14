f59027cfade94a9ecf80ab8a2ab779ecce986d1f
andreikurnosov <nixdie@yandex.ru>
2021-02-14 11:29:58 +0700

    useModal composable

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,11 @@
 <template>
+  <div class="modal" :style="style">
+    <div class="modal-background"></div>
+    <div class="modal-content">
+      <div id="modal"></div>
+    </div>
+    <button class="modal-close is-large" aria-label="close" @click="modal.hideModal"></button>
+  </div>
   <div class="section">
     <div class="container">
       <NavBar />
@@ -8,12 +15,25 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { computed, defineComponent } from 'vue'
 import NavBar from './NavBar.vue'
+import { useModal } from './useModal'
 export default defineComponent({
   name: 'App',
   components: {
     NavBar,
+  },
+  setup() {
+    const modal = useModal()
+
+    const style = computed(() => ({
+      display: modal.visible.value ? 'block' : 'none',
+    }))
+
+    return {
+      modal,
+      style
+    }
   },
 })
 </script>
--- src/NavBar.vue
+++ src/NavBar.vue
@@ -3,7 +3,21 @@
     <div class="navbar-end">
       <div class="buttons">
         <router-link class="button" to="/post/new"> New Post </router-link>
+        <button class="button" @click="modal.showModal">Signup</button>
       </div>
     </div>
+    <teleport to="#modal" v-if="modal.visible"> modal </teleport>
   </nav>
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { useModal } from './useModal'
+export default defineComponent({
+  setup() {
+    return {
+      modal: useModal(),
+    }
+  },
+})
+</script>
