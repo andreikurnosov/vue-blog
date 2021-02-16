f1c7c8a98c330ab9659114fd147e6e2f531301d8
andreikurnosov <nixdie@yandex.ru>
2021-02-16 15:28:08 +0700

    vue router navigation guards

--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from './Home.vue'
 import NewPost from './NewPost.vue'
+import { store } from './store'
 
 export const router = createRouter({
   history: createWebHistory(),
@@ -14,6 +15,19 @@ export const router = createRouter({
       name: 'NewPost',
       path: '/post/new',
       component: NewPost,
+      meta: {
+        requiresAuth: true,
+      },
     },
   ],
+})
+
+router.beforeEach((to, from, next) => {
+  if (to.meta.requiresAuth && !store.getState().authors.currentUserId) {
+    next({
+      name: 'Home',
+    })
+  } else {
+    next()
+  }
 })
